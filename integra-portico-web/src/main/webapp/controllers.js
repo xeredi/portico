eee953d0b35d2ff135992ba20dcdd0deb1e3322a
xeredi <xeredi@gmail.com>
2014-03-26 18:45:49 +0100

    AngularJS

---
+++ integra-portico-web/src/main/webapp/controllers.js
@@ -0,0 +1,12 @@
+var appControllers = angular.module('integraControllers', []);
+
+appControllers.controller('tpprsCtrl', function($http) {
+	// alert('Llamar al servidor');
+	var app = this;
+
+	$http.get("http://localhost:8080/web/maestro/tppr-listado-json.action")
+			.success(function(data) {
+				// console.log(data);
+				app.tpprs = data.tpprs;
+			});
+});
--- integra-portico-web/src/main/webapp/index.js
+++ integra-portico-web/src/main/webapp/index.js
@@ -1,24 +1,47 @@
-var integraApp = angular.module('integraApp', [ 'ngRoute' ]);
+var app = angular.module('integraApp', [ 'ngRoute', 'integraControllers' ]);
 
-integraApp.config([ '$routeProvider', function($routeProvider) {
+app.config([ '$routeProvider', function($routeProvider) {
 	// alert('URL');
-	
-	$routeProvider
-	.when('/servicio/tpsrs', {
-		templateUrl : 'partials/servicio/tpsr-listado.html'/*,
-		controller : 'PhoneListCtrl'*/
-	})
-	.when('/estadistica/peprs', {
-		templateUrl : 'partials/estadistica/pepr-listado.html'/*,
-		controller : 'PhoneListCtrl'*/
-	})
-	.when('/acceso', {
-		templateUrl : 'partials/usro-acceso.html'/*,
-		controller : 'PhoneListCtrl'*/
-	})
-	/*
-	.otherwise({
+
+	$routeProvider.when('/servicio/tpsrs', {
+		templateUrl : 'partials/servicio/tpsr-listado.html'/*
+															 * , controller :
+															 * 'PhoneListCtrl'
+															 */
+	}).when('/estadistica/peprs', {
+		templateUrl : 'partials/estadistica/pepr-listado.html'/*
+																 * , controller :
+																 * 'PhoneListCtrl'
+																 */
+	}).when('/maestro/tpprs', {
+		templateUrl : 'partials/maestro/tppr-listado.html',
+		controller : 'tpprsCtrl'
+	}).when('/metamodelo/tpprs', {
+		templateUrl : 'partials/metamodelo/tppr-listado.html'/*
+																 * , controller :
+																 * 'PhoneListCtrl'
+																 */
+	}).when('/metamodelo/tpsrs', {
+		templateUrl : 'partials/metamodelo/tpsr-listado.html'/*
+																 * , controller :
+																 * 'PhoneListCtrl'
+																 */
+	}).when('/metamodelo/tpess', {
+		templateUrl : 'partials/metamodelo/tpes-listado.html'/*
+																 * , controller :
+																 * 'PhoneListCtrl'
+																 */
+	}).when('/metamodelo/tpdts', {
+		templateUrl : 'partials/metamodelo/tpdt-listado.html'/*
+																 * , controller :
+																 * 'PhoneListCtrl'
+																 */
+	}).when('/acceso', {
+		templateUrl : 'partials/usro-acceso.html'/*
+													 * , controller :
+													 * 'PhoneListCtrl'
+													 */
+	}).otherwise({
 		redirectTo : '/acceso'
-	})
-	*/;
+	});
 } ]);
