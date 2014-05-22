de7ff7e6570d09e26be10e71bafe2cd43305fe6e
xeredi <xeredi@gmail.com>
2014-05-22 03:53:14 +0200

    PDF

--- integra-portico-business/src/main/java/xeredi/integra/model/bo/servicio/ServicioBO.java
+++ integra-portico-business/src/main/java/xeredi/integra/model/bo/servicio/ServicioBO.java
@@ -42,316 +42,331 @@ import com.google.inject.Singleton;
 @Singleton
 public class ServicioBO implements Servicio {
 
-    /** The srvc dao. */
-    @Inject
-    ServicioDAO srvcDAO;
-
-    @Inject
-    ServicioSecuenciaDAO srscDAO;
-
-    /** The srdt dao. */
-    @Inject
-    ServicioDatoDAO srdtDAO;
-
-    /** The ssrv dao. */
-    @Inject
-    SubservicioDAO ssrvDAO;
-
-    /** The ssdt dao. */
-    @Inject
-    SubservicioDatoDAO ssdtDAO;
-
-    /** The ssss dao. */
-    @Inject
-    SubservicioSubservicioDAO ssssDAO;
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    @Transactional
-    public final ServicioVO select(final Long srvcId, final String idioma) throws InstanceNotFoundException {
-        Preconditions.checkNotNull(srvcId);
-        Preconditions.checkNotNull(idioma);
-
-        final ServicioCriterioVO srvcCriterioVO = new ServicioCriterioVO();
-
-        srvcCriterioVO.setId(srvcId);
-        srvcCriterioVO.setIdioma(idioma);
-
-        final ServicioVO srvcVO = srvcDAO.selectObject(srvcCriterioVO);
-
-        if (srvcVO == null) {
-            throw new InstanceNotFoundException(ServicioVO.class.getName(), srvcId);
-        }
-
-        fillDependencies(Arrays.asList(new ServicioVO[] { srvcVO }), srvcCriterioVO, true);
-
-        return srvcVO;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    @Transactional
-    public final PaginatedList<ServicioVO> selectList(final ServicioCriterioVO srvcCriterioVO, final int offset,
-            final int limit) {
-        Preconditions.checkNotNull(srvcCriterioVO);
-
-        final int count = srvcDAO.selectCount(srvcCriterioVO);
-        final List<ServicioVO> srvcList = new ArrayList<>();
-
-        if (count > offset) {
-            srvcCriterioVO.setOffset(offset);
-            srvcCriterioVO.setLimit(limit);
-
-            srvcList.addAll(srvcDAO.selectList(srvcCriterioVO));
-
-            srvcCriterioVO.setOffset(null);
-            srvcCriterioVO.setLimit(null);
-
-            // FIXME Ojo en la paginacion, puede traer una barbaridad de
-            // dependencias
-            fillDependencies(srvcList, srvcCriterioVO, true);
-        }
-
-        return new PaginatedList<>(srvcList, offset, limit, count);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    @Transactional(executorType = ExecutorType.BATCH)
-    public final List<ServicioVO> selectList(final ServicioCriterioVO srvcCriterioVO) {
-        Preconditions.checkNotNull(srvcCriterioVO);
-
-        final List<ServicioVO> srvcList = srvcDAO.selectList(srvcCriterioVO);
-
-        // FIXME Ojo en la paginacion, puede traer una barbaridad de
-        // dependencias
-        fillDependencies(srvcList, srvcCriterioVO, false);
-
-        return srvcList;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    @Transactional
-    public final List<LabelValueVO> selectLupaList(final ServicioLupaCriterioVO srvcLupaCriterioVO) {
-        Preconditions.checkNotNull(srvcLupaCriterioVO);
-
-        return srvcDAO.selectLupaList(srvcLupaCriterioVO);
-    }
-
-    /**
-     * Fill dependencies.
-     * 
-     * @param srvcList
-     *            the srvc list
-     * @param srvcCriterioVO
-     *            the srvc criterio vo
-     * @param useIds
-     *            the use ids
-     */
-    private final void fillDependencies(final List<ServicioVO> srvcList, final ServicioCriterioVO srvcCriterioVO,
-            final boolean useIds) {
-        Preconditions.checkNotNull(srvcList);
-        Preconditions.checkNotNull(srvcCriterioVO);
-
-        if (!srvcList.isEmpty()) {
-            if (useIds) {
-                final Set<Long> srvcIds = new HashSet<>();
-
-                for (final ServicioVO srvcVO : srvcList) {
-                    srvcIds.add(srvcVO.getId());
-                }
-
-                srvcCriterioVO.setIds(srvcIds);
-            }
-
-            final List<ItemDatoVO> itdtList = srdtDAO.selectList(srvcCriterioVO);
-            final Map<Long, Map<Long, ItemDatoVO>> map = new HashMap<>();
-
-            for (final ItemDatoVO itdtVO : itdtList) {
-                if (!map.containsKey(itdtVO.getItemId())) {
-                    map.put(itdtVO.getItemId(), new HashMap<Long, ItemDatoVO>());
-                }
-
-                map.get(itdtVO.getItemId()).put(itdtVO.getTpdtId(), itdtVO);
-            }
-
-            for (final ServicioVO srvcVO : srvcList) {
-                srvcVO.setItdtMap(map.get(srvcVO.getId()));
-            }
-
-            if (useIds) {
-                srvcCriterioVO.setIds(null);
-            }
-        }
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    @Transactional(executorType = ExecutorType.BATCH)
-    public final void insert(final ServicioVO srvcVO, final List<SubservicioVO> ssrvList)
-            throws DuplicateInstanceException {
-        Preconditions.checkNotNull(srvcVO);
-        // Preconditions.checkNotNull(ssrvList);
-
-        final IgBO igBO = new IgBO();
-
-        srscDAO.incrementarSecuencia(srvcVO);
-
-        final Integer secuencia = srscDAO.obtenerSecuencia(srvcVO);
-
-        if (secuencia == null) {
-            throw new Error("No se encuentra secuencia para: " + srvcVO);
-        }
-
-        srvcVO.setNumero(ServicioVO.convertNumero(secuencia));
-
-        if (srvcDAO.exists(srvcVO)) {
-            throw new DuplicateInstanceException(ServicioVO.class.getName(), srvcVO);
-        }
-
-        srvcVO.setId(igBO.nextVal(GlobalNames.SQ_INTEGRA));
-        srvcDAO.insert(srvcVO);
-
-        if (srvcVO.getItdtMap() != null) {
-            for (final ItemDatoVO itdtVO : srvcVO.getItdtMap().values()) {
-                itdtVO.setItemId(srvcVO.getId());
-                srdtDAO.insert(itdtVO);
-            }
-        }
-
-        if (ssrvList != null) {
-            for (final SubservicioVO ssrvVO : ssrvList) {
-                ssrvVO.setId(igBO.nextVal(GlobalNames.SQ_INTEGRA));
-                ssrvVO.setSrvc(srvcVO);
-                ssrvDAO.insert(ssrvVO);
-            }
-
-            for (final SubservicioVO ssrvVO : ssrvList) {
-                if (ssrvVO.getItdtMap() != null) {
-                    for (final ItemDatoVO itdtVO : ssrvVO.getItdtMap().values()) {
-                        itdtVO.setItemId(ssrvVO.getId());
-                        ssdtDAO.insert(itdtVO);
-                    }
-                }
-            }
-        }
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    @Transactional(executorType = ExecutorType.BATCH)
-    public final void update(final ServicioVO srvcVO) {
-        Preconditions.checkNotNull(srvcVO);
-
-        for (final ItemDatoVO itdtVO : srvcVO.getItdtMap().values()) {
-            srdtDAO.update(itdtVO);
-        }
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    @Transactional(executorType = ExecutorType.BATCH)
-    public final void duplicate(final ServicioVO srvcVO) {
-        Preconditions.checkNotNull(srvcVO);
-
-        final IgBO igBO = new IgBO();
-        final Map<Long, Long> ssrvIds = new HashMap<>();
-
-        // Busqueda de los elementos a duplicar
-        final ServicioCriterioVO srvcCriterioVO = new ServicioCriterioVO();
-        final SubservicioCriterioVO ssrvCriterioVO = new SubservicioCriterioVO();
-
-        srvcCriterioVO.setId(srvcVO.getId());
-        ssrvCriterioVO.setSrvc(srvcCriterioVO);
-
-        final List<SubservicioVO> ssrvList = ssrvDAO.selectList(ssrvCriterioVO);
-        final List<ItemDatoVO> ssdtList = ssdtDAO.selectList(ssrvCriterioVO);
-        final List<SubservicioSubservicioVO> ssssList = ssssDAO.selectList(ssrvCriterioVO);
-
-        // Duplicado del servicio. Se duplica el propio servicio y sus datos asociados, los
-        // subservicios y
-        // los datos asociados, y las relaciones entre subservicios.
-        srscDAO.incrementarSecuencia(srvcVO);
-
-        final Integer secuencia = srscDAO.obtenerSecuencia(srvcVO);
-        final Long srvcIdActual = srvcVO.getId();
-
-        if (secuencia == null) {
-            throw new Error("No se encuentra secuencia para: " + srvcVO.getEtiqueta());
-        }
-
-        srvcVO.setId(igBO.nextVal(GlobalNames.SQ_INTEGRA));
-        srvcVO.setNumero(ServicioVO.convertNumero(secuencia));
-
-        srvcDAO.insert(srvcVO);
-
-        for (final SubservicioVO ssrvVO : ssrvList) {
-            final Long ssrvIdActual = ssrvVO.getId();
-
-            ssrvVO.setId(igBO.nextVal(GlobalNames.SQ_INTEGRA));
-            ssrvVO.setSrvc(srvcVO);
-            ssrvDAO.insert(ssrvVO);
-
-            ssrvIds.put(ssrvIdActual, ssrvVO.getId());
-        }
-
-        for (final ItemDatoVO itdtVO : srvcVO.getItdtMap().values()) {
-            itdtVO.setItemId(srvcVO.getId());
-            srdtDAO.insert(itdtVO);
-        }
-
-        for (final ItemDatoVO ssdtVO : ssdtList) {
-            ssdtVO.setItemId(ssrvIds.get(ssdtVO.getItemId()));
-            ssdtDAO.insert(ssdtVO);
-        }
-
-        for (final SubservicioSubservicioVO ssssVO : ssssList) {
-            ssssDAO.insert(new SubservicioSubservicioVO(ssrvIds.get(ssssVO.getSsrvPadreId()), ssrvIds.get(ssssVO
-                    .getSsrvHijoId())));
-        }
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    @Transactional
-    public final void delete(final Long srvcId) throws InstanceNotFoundException {
-        Preconditions.checkNotNull(srvcId);
-
-        final ServicioCriterioVO srvcCriterioVO = new ServicioCriterioVO();
-        final SubservicioCriterioVO ssrvCriterioVO = new SubservicioCriterioVO();
-
-        srvcCriterioVO.setId(srvcId);
-        ssrvCriterioVO.setSrvc(srvcCriterioVO);
-
-        ssssDAO.delete(ssrvCriterioVO);
-
-        ssdtDAO.delete(ssrvCriterioVO);
-        ssrvDAO.delete(ssrvCriterioVO);
-
-        srdtDAO.delete(srvcCriterioVO);
-
-        final int updated = srvcDAO.delete(srvcId);
-
-        if (updated == 0) {
-            throw new InstanceNotFoundException(ServicioVO.class.getName(), srvcId);
-        }
-    }
+	/** The srvc dao. */
+	@Inject
+	ServicioDAO srvcDAO;
+
+	@Inject
+	ServicioSecuenciaDAO srscDAO;
+
+	/** The srdt dao. */
+	@Inject
+	ServicioDatoDAO srdtDAO;
+
+	/** The ssrv dao. */
+	@Inject
+	SubservicioDAO ssrvDAO;
+
+	/** The ssdt dao. */
+	@Inject
+	SubservicioDatoDAO ssdtDAO;
+
+	/** The ssss dao. */
+	@Inject
+	SubservicioSubservicioDAO ssssDAO;
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	@Transactional
+	public final ServicioVO select(final Long srvcId, final String idioma)
+			throws InstanceNotFoundException {
+		Preconditions.checkNotNull(srvcId);
+		Preconditions.checkNotNull(idioma);
+
+		final ServicioCriterioVO srvcCriterioVO = new ServicioCriterioVO();
+
+		srvcCriterioVO.setId(srvcId);
+		srvcCriterioVO.setIdioma(idioma);
+
+		final ServicioVO srvcVO = srvcDAO.selectObject(srvcCriterioVO);
+
+		if (srvcVO == null) {
+			throw new InstanceNotFoundException(ServicioVO.class.getName(),
+					srvcId);
+		}
+
+		fillDependencies(Arrays.asList(new ServicioVO[] { srvcVO }),
+				srvcCriterioVO, true);
+
+		return srvcVO;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	@Transactional
+	public final PaginatedList<ServicioVO> selectList(
+			final ServicioCriterioVO srvcCriterioVO, final int offset,
+			final int limit) {
+		Preconditions.checkNotNull(srvcCriterioVO);
+
+		final int count = srvcDAO.selectCount(srvcCriterioVO);
+		final List<ServicioVO> srvcList = new ArrayList<>();
+
+		if (count > offset) {
+			srvcCriterioVO.setOffset(offset);
+			srvcCriterioVO.setLimit(limit);
+
+			srvcList.addAll(srvcDAO.selectList(srvcCriterioVO));
+
+			srvcCriterioVO.setOffset(null);
+			srvcCriterioVO.setLimit(null);
+
+			// FIXME Ojo en la paginacion, puede traer una barbaridad de
+			// dependencias
+			fillDependencies(srvcList, srvcCriterioVO, true);
+		}
+
+		return new PaginatedList<>(srvcList, offset, limit, count);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	@Transactional(executorType = ExecutorType.BATCH)
+	public final List<ServicioVO> selectList(
+			final ServicioCriterioVO srvcCriterioVO) {
+		Preconditions.checkNotNull(srvcCriterioVO);
+
+		final List<ServicioVO> srvcList = srvcDAO.selectList(srvcCriterioVO);
+
+		// FIXME Ojo en la paginacion, puede traer una barbaridad de
+		// dependencias
+		fillDependencies(srvcList, srvcCriterioVO,
+				srvcCriterioVO.getLimit() != null);
+
+		return srvcList;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	@Transactional
+	public final List<LabelValueVO> selectLupaList(
+			final ServicioLupaCriterioVO srvcLupaCriterioVO) {
+		Preconditions.checkNotNull(srvcLupaCriterioVO);
+
+		return srvcDAO.selectLupaList(srvcLupaCriterioVO);
+	}
+
+	/**
+	 * Fill dependencies.
+	 * 
+	 * @param srvcList
+	 *            the srvc list
+	 * @param srvcCriterioVO
+	 *            the srvc criterio vo
+	 * @param useIds
+	 *            the use ids
+	 */
+	private final void fillDependencies(final List<ServicioVO> srvcList,
+			final ServicioCriterioVO srvcCriterioVO, final boolean useIds) {
+		Preconditions.checkNotNull(srvcList);
+		Preconditions.checkNotNull(srvcCriterioVO);
+
+		if (!srvcList.isEmpty()) {
+			if (useIds) {
+				final Set<Long> srvcIds = new HashSet<>();
+
+				for (final ServicioVO srvcVO : srvcList) {
+					srvcIds.add(srvcVO.getId());
+				}
+
+				srvcCriterioVO.setIds(srvcIds);
+			}
+
+			final List<ItemDatoVO> itdtList = srdtDAO
+					.selectList(srvcCriterioVO);
+			final Map<Long, Map<Long, ItemDatoVO>> map = new HashMap<>();
+
+			for (final ItemDatoVO itdtVO : itdtList) {
+				if (!map.containsKey(itdtVO.getItemId())) {
+					map.put(itdtVO.getItemId(), new HashMap<Long, ItemDatoVO>());
+				}
+
+				map.get(itdtVO.getItemId()).put(itdtVO.getTpdtId(), itdtVO);
+			}
+
+			for (final ServicioVO srvcVO : srvcList) {
+				srvcVO.setItdtMap(map.get(srvcVO.getId()));
+			}
+
+			if (useIds) {
+				srvcCriterioVO.setIds(null);
+			}
+		}
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	@Transactional(executorType = ExecutorType.BATCH)
+	public final void insert(final ServicioVO srvcVO,
+			final List<SubservicioVO> ssrvList)
+					throws DuplicateInstanceException {
+		Preconditions.checkNotNull(srvcVO);
+		// Preconditions.checkNotNull(ssrvList);
+
+		final IgBO igBO = new IgBO();
+
+		srscDAO.incrementarSecuencia(srvcVO);
+
+		final Integer secuencia = srscDAO.obtenerSecuencia(srvcVO);
+
+		if (secuencia == null) {
+			throw new Error("No se encuentra secuencia para: " + srvcVO);
+		}
+
+		srvcVO.setNumero(ServicioVO.convertNumero(secuencia));
+
+		if (srvcDAO.exists(srvcVO)) {
+			throw new DuplicateInstanceException(ServicioVO.class.getName(),
+					srvcVO);
+		}
+
+		srvcVO.setId(igBO.nextVal(GlobalNames.SQ_INTEGRA));
+		srvcDAO.insert(srvcVO);
+
+		if (srvcVO.getItdtMap() != null) {
+			for (final ItemDatoVO itdtVO : srvcVO.getItdtMap().values()) {
+				itdtVO.setItemId(srvcVO.getId());
+				srdtDAO.insert(itdtVO);
+			}
+		}
+
+		if (ssrvList != null) {
+			for (final SubservicioVO ssrvVO : ssrvList) {
+				ssrvVO.setId(igBO.nextVal(GlobalNames.SQ_INTEGRA));
+				ssrvVO.setSrvc(srvcVO);
+				ssrvDAO.insert(ssrvVO);
+			}
+
+			for (final SubservicioVO ssrvVO : ssrvList) {
+				if (ssrvVO.getItdtMap() != null) {
+					for (final ItemDatoVO itdtVO : ssrvVO.getItdtMap().values()) {
+						itdtVO.setItemId(ssrvVO.getId());
+						ssdtDAO.insert(itdtVO);
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	@Transactional(executorType = ExecutorType.BATCH)
+	public final void update(final ServicioVO srvcVO) {
+		Preconditions.checkNotNull(srvcVO);
+
+		for (final ItemDatoVO itdtVO : srvcVO.getItdtMap().values()) {
+			srdtDAO.update(itdtVO);
+		}
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	@Transactional(executorType = ExecutorType.BATCH)
+	public final void duplicate(final ServicioVO srvcVO) {
+		Preconditions.checkNotNull(srvcVO);
+
+		final IgBO igBO = new IgBO();
+		final Map<Long, Long> ssrvIds = new HashMap<>();
+
+		// Busqueda de los elementos a duplicar
+		final ServicioCriterioVO srvcCriterioVO = new ServicioCriterioVO();
+		final SubservicioCriterioVO ssrvCriterioVO = new SubservicioCriterioVO();
+
+		srvcCriterioVO.setId(srvcVO.getId());
+		ssrvCriterioVO.setSrvc(srvcCriterioVO);
+
+		final List<SubservicioVO> ssrvList = ssrvDAO.selectList(ssrvCriterioVO);
+		final List<ItemDatoVO> ssdtList = ssdtDAO.selectList(ssrvCriterioVO);
+		final List<SubservicioSubservicioVO> ssssList = ssssDAO
+				.selectList(ssrvCriterioVO);
+
+		// Duplicado del servicio. Se duplica el propio servicio y sus datos
+		// asociados, los
+		// subservicios y
+		// los datos asociados, y las relaciones entre subservicios.
+		srscDAO.incrementarSecuencia(srvcVO);
+
+		final Integer secuencia = srscDAO.obtenerSecuencia(srvcVO);
+		final Long srvcIdActual = srvcVO.getId();
+
+		if (secuencia == null) {
+			throw new Error("No se encuentra secuencia para: "
+					+ srvcVO.getEtiqueta());
+		}
+
+		srvcVO.setId(igBO.nextVal(GlobalNames.SQ_INTEGRA));
+		srvcVO.setNumero(ServicioVO.convertNumero(secuencia));
+
+		srvcDAO.insert(srvcVO);
+
+		for (final SubservicioVO ssrvVO : ssrvList) {
+			final Long ssrvIdActual = ssrvVO.getId();
+
+			ssrvVO.setId(igBO.nextVal(GlobalNames.SQ_INTEGRA));
+			ssrvVO.setSrvc(srvcVO);
+			ssrvDAO.insert(ssrvVO);
+
+			ssrvIds.put(ssrvIdActual, ssrvVO.getId());
+		}
+
+		for (final ItemDatoVO itdtVO : srvcVO.getItdtMap().values()) {
+			itdtVO.setItemId(srvcVO.getId());
+			srdtDAO.insert(itdtVO);
+		}
+
+		for (final ItemDatoVO ssdtVO : ssdtList) {
+			ssdtVO.setItemId(ssrvIds.get(ssdtVO.getItemId()));
+			ssdtDAO.insert(ssdtVO);
+		}
+
+		for (final SubservicioSubservicioVO ssssVO : ssssList) {
+			ssssDAO.insert(new SubservicioSubservicioVO(ssrvIds.get(ssssVO
+					.getSsrvPadreId()), ssrvIds.get(ssssVO.getSsrvHijoId())));
+		}
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	@Transactional
+	public final void delete(final Long srvcId)
+			throws InstanceNotFoundException {
+		Preconditions.checkNotNull(srvcId);
+
+		final ServicioCriterioVO srvcCriterioVO = new ServicioCriterioVO();
+		final SubservicioCriterioVO ssrvCriterioVO = new SubservicioCriterioVO();
+
+		srvcCriterioVO.setId(srvcId);
+		ssrvCriterioVO.setSrvc(srvcCriterioVO);
+
+		ssssDAO.delete(ssrvCriterioVO);
+
+		ssdtDAO.delete(ssrvCriterioVO);
+		ssrvDAO.delete(ssrvCriterioVO);
+
+		srdtDAO.delete(srvcCriterioVO);
+
+		final int updated = srvcDAO.delete(srvcId);
+
+		if (updated == 0) {
+			throw new InstanceNotFoundException(ServicioVO.class.getName(),
+					srvcId);
+		}
+	}
 
 }
---
+++ integra-portico-business/src/main/java/xeredi/integra/model/bo/util/pdf/ServicioPdf.java
@@ -0,0 +1,125 @@
+package xeredi.integra.model.bo.util.pdf;
+
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import net.sf.dynamicreports.jasper.builder.JasperReportBuilder;
+import net.sf.dynamicreports.report.builder.DynamicReports;
+import net.sf.dynamicreports.report.builder.component.Components;
+import net.sf.dynamicreports.report.builder.component.HorizontalListBuilder;
+import net.sf.dynamicreports.report.constant.PageOrientation;
+import net.sf.dynamicreports.report.constant.PageType;
+import net.sf.dynamicreports.report.exception.DRException;
+import xeredi.integra.model.vo.comun.ItemDatoVO;
+import xeredi.integra.model.vo.metamodelo.EntidadTipoDatoVO;
+import xeredi.integra.model.vo.metamodelo.TipoElemento;
+import xeredi.integra.model.vo.metamodelo.TipoServicioVO;
+import xeredi.integra.model.vo.metamodelo.TipoSubservicioVO;
+import xeredi.integra.model.vo.servicio.ServicioVO;
+import xeredi.integra.model.vo.servicio.SubservicioVO;
+
+import com.google.common.base.Preconditions;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class ServicioPdf.
+ */
+public final class ServicioPdf extends BasePdf {
+
+	/**
+	 * Instantiates a new servicio pdf.
+	 * 
+	 * @param alocale
+	 *            the alocale
+	 */
+	public ServicioPdf(final Locale alocale) {
+		super(alocale);
+	}
+
+	public void imprimir(final ServicioVO srvcVO, final TipoServicioVO tpsrVO,
+			final Map<Long, TipoSubservicioVO> entiHijasMap,
+			final Map<Long, List<SubservicioVO>> itemHijosMap,
+			final OutputStream stream) throws DRException {
+		Preconditions.checkNotNull(srvcVO);
+		Preconditions.checkNotNull(tpsrVO);
+
+		final List<List<PdfCell>> listCells = new ArrayList<>();
+
+		List<PdfCell> rowCells = new ArrayList<>();
+		int accWidth = 0;
+
+		rowCells.add(new PdfCell(tpsrVO.getNombre(), srvcVO.getEtiqueta(), 4,
+				TipoElemento.TX));
+
+		if (tpsrVO.isTemporal()) {
+			rowCells.add(new PdfCell("F. Inicio", PdfConstants.DATE_FORMAT
+					.format(srvcVO.getFinicio()), 4, TipoElemento.FE));
+			rowCells.add(new PdfCell("F. Fin", srvcVO.getFfin() == null ? ""
+					: PdfConstants.DATE_FORMAT.format(srvcVO.getFfin()), 4,
+					TipoElemento.FE));
+		}
+
+		listCells.add(rowCells);
+
+		rowCells = new ArrayList<>();
+		accWidth = 0;
+
+		for (final Long tpdtId : tpsrVO.getEntdList()) {
+			final EntidadTipoDatoVO entdVO = tpsrVO.getEntdMap().get(tpdtId);
+			final ItemDatoVO itdtVO = srvcVO.getItdtMap().get(tpdtId);
+
+			if (accWidth + entdVO.getSpan() > PdfConstants.MAX_SPAN) {
+				listCells.add(rowCells);
+				rowCells = new ArrayList<>();
+				accWidth = 0;
+			}
+
+			rowCells.add(new PdfCell(entdVO.getEtiqueta(), getItdtValue(entdVO,
+					itdtVO), entdVO.getSpan(), entdVO.getTpdt()
+					.getTipoElemento()));
+			accWidth += entdVO.getSpan();
+		}
+
+		if (!rowCells.isEmpty()) {
+			listCells.add(rowCells);
+		}
+
+		final HorizontalListBuilder list = DynamicReports.cmp.horizontalList();
+
+		for (final List<PdfCell> row : listCells) {
+			for (final PdfCell pdfCell : row) {
+				list.add(getFieldLabel(pdfCell));
+			}
+
+			list.newRow();
+
+			for (final PdfCell pdfCell : row) {
+				list.add(getFieldValue(pdfCell));
+			}
+
+			list.newRow();
+		}
+
+		final JasperReportBuilder report = DynamicReports.report();
+
+		report.setPageFormat(PageType.A4, PageOrientation.LANDSCAPE);
+		report.addTitle(DynamicReports.cmp.text(tpsrVO.getNombre()).setStyle(
+				PdfConstants.H1_STYLE));
+		report.addTitle(list, Components.pageBreak());
+
+		// if (tpprVO.getEntiHijasList() != null) {
+		// for (final Long entiId : tpprVO.getEntiHijasList()) {
+		// if (!itemHijosMap.get(entiId).isEmpty()) {
+		// report.addTitle(DynamicReports.cmp.subreport(getSubreport(
+		// entiHijasMap.get(entiId), itemHijosMap.get(entiId))));
+		// }
+		// }
+		// }
+
+		report.toPdf(stream);
+	}
+
+}
