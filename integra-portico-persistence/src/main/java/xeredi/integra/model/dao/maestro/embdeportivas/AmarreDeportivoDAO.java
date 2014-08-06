ecac3a5b4661284967c132f724dac3b1297c2d68
xeredi <xeredi@gmail.com>
2014-08-06 11:51:47 +0200

    Embarcaciones Deportivas

---
+++ integra-portico-persistence/src/main/java/xeredi/integra/model/dao/maestro/embdeportivas/AmarreDeportivoDAO.java
@@ -0,0 +1,19 @@
+package xeredi.integra.model.dao.maestro.embdeportivas;
+
+import xeredi.integra.model.vo.maestro.ParametroCriterioVO;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Interface AmarreDeportivoDAO.
+ */
+public interface AmarreDeportivoDAO {
+
+    /**
+     * Update recalcular estado.
+     *
+     * @param prmtCriterioVO
+     *            the prmt criterio vo
+     * @return the int
+     */
+    int updateRecalcularEstado(final ParametroCriterioVO prmtCriterioVO);
+}
