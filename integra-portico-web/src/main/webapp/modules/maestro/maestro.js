de49edbdc975653a9fb0449f982c3d8063f4aec9
xeredi <xeredi@gmail.com>
2014-08-29 12:13:54 +0200

    Maestro - UI

--- integra-portico-web/src/main/webapp/modules/maestro/maestro.js
+++ integra-portico-web/src/main/webapp/modules/maestro/maestro.js
@@ -56,121 +56,105 @@ angular.module('maestro', [ 'ui.router' ])
 
             .state('prmts.detalle', {
                 url : '/detalle/:itemId',
-                views : {
-                    '' : {
-                        templateUrl : 'modules/maestro/prmt-detalle.html',
-                        controller : function($http, $scope, $state, $stateParams) {
-                            var url = "maestro/prmt-detalle.action?item.id=" + $stateParams.itemId;
+                templateUrl : 'modules/maestro/prmt-detalle.html',
+                controller : function($http, $scope, $state, $stateParams) {
+                    var url = "maestro/prmt-detalle.action?item.id=" + $stateParams.itemId;
 
-                            $http.get(url).success(function(data) {
-                                $scope.enti = data.enti;
-                                $scope.item = data.item;
-                                $scope.p18nMap = data.p18nMap;
-                                $scope.entiHijasList = data.entiHijasList;
-                                $scope.itemHijosMap = data.itemHijosMap;
-                                $scope.availableLanguages = data.availableLanguages;
-                            });
-                        }
-                    }
+                    $http.get(url).success(function(data) {
+                        $scope.enti = data.enti;
+                        $scope.item = data.item;
+                        $scope.p18nMap = data.p18nMap;
+                        $scope.entiHijasList = data.entiHijasList;
+                        $scope.itemHijosMap = data.itemHijosMap;
+                        $scope.availableLanguages = data.availableLanguages;
+                    });
                 }
             })
 
             .state('prmts.alta', {
                 url : '/alta/:entiId',
-                views : {
-                    '' : {
-                        templateUrl : 'modules/maestro/prmt-edicion.html',
-                        controller : function($http, $scope, $state, $stateParams) {
-                            var url = "maestro/prmt-crear.action?item.entiId=" + $stateParams.entiId;
+                templateUrl : 'modules/maestro/prmt-edicion.html',
+                controller : function($http, $scope, $state, $stateParams) {
+                    var url = "maestro/prmt-crear.action?item.entiId=" + $stateParams.entiId;
 
-                            $http.get(url).success(function(data) {
-                                $scope.accion = data.accion;
-                                $scope.availableLanguages = data.availableLanguages;
-                                $scope.enti = data.enti;
-                                $scope.item = data.item;
-                                $scope.p18nMap = data.p18nMap;
-                                $scope.labelValuesMap = data.labelValuesMap;
-                            });
+                    $http.get(url).success(function(data) {
+                        $scope.accion = data.accion;
+                        $scope.availableLanguages = data.availableLanguages;
+                        $scope.enti = data.enti;
+                        $scope.item = data.item;
+                        $scope.p18nMap = data.p18nMap;
+                        $scope.labelValuesMap = data.labelValuesMap;
+                    });
 
-                            $scope.guardar = function() {
-                                console.log($scope.item);
-                                console.log($scope.p18nMap);
-                                console.log($scope.accion);
+                    $scope.guardar = function() {
+                        console.log($scope.item);
+                        console.log($scope.p18nMap);
+                        console.log($scope.accion);
 
-                                $http.post("maestro/prmt-guardar.action", {
-                                    item : $scope.item,
-                                    p18nMap : $scope.p18nMap,
-                                    accion : $scope.accion
-                                });
+                        $http.post("maestro/prmt-guardar.action", {
+                            item : $scope.item,
+                            p18nMap : $scope.p18nMap,
+                            accion : $scope.accion
+                        });
 
-                                // $state.go('prmts.detalle', $stateParams);
-                            };
-                        }
-                    }
+                        // $state.go('prmts.detalle', $stateParams);
+                    };
                 }
             })
 
             .state('prmts.editar', {
                 url : '/editar/:itemId',
-                views : {
-                    '' : {
-                        templateUrl : 'modules/maestro/prmt-edicion.html',
-                        controller : function($http, $scope, $stateParams) {
-                            var url = "maestro/prmt-editar.action?item.id=" + $stateParams.itemId;
+                templateUrl : 'modules/maestro/prmt-edicion.html',
+                controller : function($http, $scope, $stateParams) {
+                    var url = "maestro/prmt-editar.action?item.id=" + $stateParams.itemId;
 
-                            $http.get(url).success(function(data) {
-                                $scope.accion = data.accion;
-                                $scope.availableLanguages = data.availableLanguages;
-                                $scope.enti = data.enti;
-                                $scope.item = data.item;
-                                $scope.p18nMap = data.p18nMap;
-                                $scope.entiHijasList = data.entiHijasList;
-                                $scope.itemHijosMap = data.itemHijosMap;
-                                $scope.labelValuesMap = data.labelValuesMap;
-                            });
+                    $http.get(url).success(function(data) {
+                        $scope.accion = data.accion;
+                        $scope.availableLanguages = data.availableLanguages;
+                        $scope.enti = data.enti;
+                        $scope.item = data.item;
+                        $scope.p18nMap = data.p18nMap;
+                        $scope.entiHijasList = data.entiHijasList;
+                        $scope.itemHijosMap = data.itemHijosMap;
+                        $scope.labelValuesMap = data.labelValuesMap;
+                    });
 
-                            $scope.guardar = function() {
-                                console.log($scope.item);
-                                console.log($scope.p18nMap);
-                                console.log($scope.accion);
+                    $scope.guardar = function() {
+                        console.log($scope.item);
+                        console.log($scope.p18nMap);
+                        console.log($scope.accion);
 
-                                $http.post("maestro/prmt-guardar.action", {
-                                    item : $scope.item,
-                                    p18nMap : $scope.p18nMap,
-                                    accion : $scope.accion
-                                }).success(function(data) {
-                                    alert('Success')
-                                }).error(function(data) {
-                                    alert('Error')
-                                });
+                        $http.post("maestro/prmt-guardar.action", {
+                            item : $scope.item,
+                            p18nMap : $scope.p18nMap,
+                            accion : $scope.accion
+                        }).success(function(data) {
+                            alert('Success')
+                        }).error(function(data) {
+                            alert('Error')
+                        });
 
-                                // $state.go('prmts.detalle', $stateParams);
-                            };
-                        }
-                    }
+                        // $state.go('prmts.detalle', $stateParams);
+                    };
                 }
             })
 
             .state('prmts.duplicar', {
                 url : '/duplicar/:itemId',
-                views : {
-                    '' : {
-                        templateUrl : 'modules/maestro/prmt-edicion.html',
-                        controller : function($http, $scope, $stateParams) {
-                            var url = "maestro/prmt-duplicar.action?item.id=" + $stateParams.itemId;
+                templateUrl : 'modules/maestro/prmt-edicion.html',
+                controller : function($http, $scope, $stateParams) {
+                    var url = "maestro/prmt-duplicar.action?item.id=" + $stateParams.itemId;
 
-                            $http.get(url).success(function(data) {
-                                $scope.accion = data.accion;
-                                $scope.availableLanguages = data.availableLanguages;
-                                $scope.enti = data.enti;
-                                $scope.item = data.item;
-                                $scope.p18nMap = data.p18nMap;
-                                $scope.entiHijasList = data.entiHijasList;
-                                $scope.itemHijosMap = data.itemHijosMap;
-                                $scope.labelValuesMap = data.labelValuesMap;
-                            });
-                        }
-                    }
+                    $http.get(url).success(function(data) {
+                        $scope.accion = data.accion;
+                        $scope.availableLanguages = data.availableLanguages;
+                        $scope.enti = data.enti;
+                        $scope.item = data.item;
+                        $scope.p18nMap = data.p18nMap;
+                        $scope.entiHijasList = data.entiHijasList;
+                        $scope.itemHijosMap = data.itemHijosMap;
+                        $scope.labelValuesMap = data.labelValuesMap;
+                    });
                 }
             })
         })
