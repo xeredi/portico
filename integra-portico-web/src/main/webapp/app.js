c28758db1f8e8049703f7f61baaac1c896396123
xeredi <xeredi@gmail.com>
2014-10-14 07:00:41 +0200

    Cache IE

--- integra-portico-web/src/main/webapp/app.js
+++ integra-portico-web/src/main/webapp/app.js
@@ -12,6 +12,16 @@ app.config([ "$routeProvider", function($routeProvider) {
     })
 } ]);
 
+app.config([ '$httpProvider', function($httpProvider) {
+    // initialize get if not there
+    if (!$httpProvider.defaults.headers.get) {
+        $httpProvider.defaults.headers.get = {};
+    }
+    // disable IE ajax request caching
+    $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
+    $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
+} ]);
+
 app.run([ '$location', '$rootScope', function($location, $rootScope) {
     $rootScope.$on('$routeChangeSuccess', function(event, current, previous) {
         $rootScope.title = current.$$route.title;
