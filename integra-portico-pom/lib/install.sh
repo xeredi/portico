80f576ee9ab2428b2120178f060163cc5cb2595f
xeredi <xeredi@gmail.com>
2015-11-18 13:33:48 +0100

    Maven - Reorganize Libraries

--- integra-portico-pom/lib/install.sh
+++ integra-portico-pom/lib/install.sh
@@ -1,1 +1,2 @@
 mvn install:install-file -Dfile=ojdbc6.jar -DgroupId=com.oracle -DartifactId=jdbc -Dversion=11.2 -Dpackaging=jar
+mvn install:install-file -Dfile=ojdbc7.jar -DgroupId=com.oracle -DartifactId=jdbc -Dversion=12.1 -Dpackaging=jar
