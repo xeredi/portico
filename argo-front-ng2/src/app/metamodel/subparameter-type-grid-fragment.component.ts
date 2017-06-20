edd678e40be84a803c36d9e34b8f0f0cf919d26e
xeredi <xeredi@gmail.com>
2017-06-20 06:13:21 +0200

    Stable

--- argo-front-ng2/src/app/metamodel/subparameter-type-grid-fragment.component.ts
+++ argo-front-ng2/src/app/metamodel/subparameter-type-grid-fragment.component.ts
@@ -10,10 +10,6 @@ import { Component, Input } from '@angular/core';
                 <th nowrap="nowrap"></th>
                 <th nowrap="nowrap">Código</th>
                 <th nowrap="nowrap">Nombre</th>
-                <th nowrap="nowrap">Alta?</th>
-                <th nowrap="nowrap">Baja?</th>
-                <th nowrap="nowrap">Edicion?</th>
-                <th nowrap="nowrap">Duplicado?</th>
                 <th nowrap="nowrap">Grid Máx (filas)</th>
                 <th nowrap="nowrap">Maestro</th>
                 <th nowrap="nowrap">Classpath</th>
@@ -24,11 +20,6 @@ import { Component, Input } from '@angular/core';
                 <td><a [routerLink]="['/metamodel/subparameter-type/detail', item.id]"><i class="fa fa-search"></i></a></td>
                 <td nowrap="nowrap" [innerHTML]="item.codigo"></td>
                 <td nowrap="nowrap" [innerHTML]="item.nombre"></td>
-                <td nowrap="nowrap"><i class="fa" [ngClass]="{true:'fa-check', false:'fa-remove'}[item.cmdAlta]"></i></td>
-                <td nowrap="nowrap"><i class="fa" [ngClass]="{true:'fa-check', false:'fa-remove'}[item.cmdBaja]"></i></td>
-                <td nowrap="nowrap"><i class="fa" [ngClass]="{true:'fa-check', false:'fa-remove'}[item.cmdEdicion]"></i></td>
-                <td nowrap="nowrap"><i class="fa"
-                    [ngClass]="{true:'fa-check', false:'fa-remove'}[item.cmdDuplicado]"></i></td>
                 <td nowrap="nowrap" [innerHTML]="item.maxGrid" class="number"></td>
                 <td nowrap="nowrap" [innerHTML]="item.tpprAsociado.nombre"></td>
                 <td nowrap="nowrap" [innerHTML]="item.classpath"></td>
